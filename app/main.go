d264b3ad493aea960a610cdbadc3f73fa614508f
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-12-26 07:55:10 -0300

    pong

--- app/main.go
+++ app/main.go
@@ -12,9 +12,27 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		os.Exit(1)
+	}
+	request := string(buf[:n])
+	_ = request
+
+	n, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
 }
