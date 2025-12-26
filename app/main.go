7e67a3f4816d96767cac949cfec36c5bfee3ccc1
LucasGdosR <lucas.gribel.reis@gmail.com>
2025-12-26 09:10:43 -0300

    echo

--- app/main.go
+++ app/main.go
@@ -4,7 +4,28 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type request struct {
+	// argc is redundant, as it is just len(args) - 1
+	argc int
+	command
+	args []string
+}
+
+type command int
+
+const (
+	ping command = iota + 1
+	echo
+)
+
+var commands_map = map[string]command{
+	"PING": ping,
+	"ECHO": echo,
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -33,10 +54,8 @@ func main() {
 						os.Exit(1)
 					}
 				}
-				request := string(buf[:n])
-				_ = request
 
-				n, err = conn.Write([]byte("+PONG\r\n"))
+				n, err = conn.Write(dispatch(parse_request(string(buf[:n]))))
 				if err != nil {
 					fmt.Println("Error writing response: ", err.Error())
 					os.Exit(1)
@@ -44,4 +63,46 @@ func main() {
 			}
 		}()
 	}
+}
+
+// This function could be defensive, double checking that every specified length matches the lenght,
+// and it could also be efficient by parsing only the lengths and manualy slicing the `r` string
+// instead of looking for CRLF. For now it is simple.
+func parse_request(r string) request {
+	tokens := strings.Split(r, "\r\n")
+	// TODO: check it starts with '*'. Handle Atoi error.
+	argc, _ := strconv.Atoi(tokens[0][1:])
+	if argc == 0 {
+		// TODO: return "empty request"
+		return request{}
+	}
+	com, ok := parse_command(tokens[2])
+	if !ok {
+		fmt.Println("Error parsing command: command not found.")
+		// TODO: return "command not supported"
+		return request{}
+	}
+	args := make([]string, 0, argc-1)
+	for i := 4; i < len(tokens); i += 2 {
+		args = append(args, tokens[i])
+	}
+	return request{argc: argc, command: com, args: args}
+}
+
+func parse_command(s string) (command, bool) {
+	c, ok := commands_map[strings.ToUpper(s)]
+	return c, ok
+}
+
+func dispatch(r request) []byte {
+	var response string
+	switch r.command {
+	case ping:
+		response = "+PONG\r\n"
+	case echo:
+		response = fmt.Sprintf("$%v\r\n%v\r\n", len(r.args[0]), r.args[0])
+	default:
+		response = "+Command not found.\r\n"
+	}
+	return []byte(response)
 }
